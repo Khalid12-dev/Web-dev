afe74ce7c3c2d159e5a1acf58016cc55a1934715
Khalid12-dev <khalidhussainkh2001@gmail.com>
2025-11-08 22:50:18 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+// import Navbar from './Components/Navbar';
+// import About from './Components/About';
+import Task from './Components/Task';
+import './App.css';
+
+function App() {
+  return (
+   <>
+ <Task/>
+   </>
+   
+  );
+}
+
+export default App;
